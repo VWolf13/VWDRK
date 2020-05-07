3caf2a750569bb92916f7bd38efc18cbf42dead6
VWolf <58598786+VWolf13@users.noreply.github.com>
2020-05-07 04:35:31 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,8 @@
 import os
+os.system('clear') 
 os.system('dos2unix vwdrk.sh')
 os.system('pkg install bash')
+os.system('pkg install zip')
 os.system('clear')
 os.system('sleep 0.8')
 os.system('bash vwdrk.sh')
